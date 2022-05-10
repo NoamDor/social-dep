9a3aac6870773076e374864aac9332334d8027d4
Jonathan Carse <jonathan@vhive.ai>
2022-05-10 10:42:22 -0400

    server without db

--- social-dep-server/server.js
+++ social-dep-server/server.js
@@ -1,35 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+const jsonParser = bodyParser.json()
+
+const requests = [{
+    id: 0,
+    creatorName: 'creator',
+    clientName: 'client',
+    clientId: '123456789',
+    phoneNum: '0547776666',
+    address: '123 main street',
+    desc: 'description',
+    decisions: 'decisions',
+    status: 'new',
+    category: 'elderly'
+}];
+
+let id = 1;
+
 app.get('/request', function(req, res) {
-    res.json([{
-        id: 1,
-        creatorName: 'creator',
-        clientName: 'client',
-        clientId: '123456789',
-        phoneNum: '0547776666',
-        address: '123 main street',
-        desc: 'description',
-        decisions: 'decisions',
-        status: 'new',
-        category: 'elderly'
-    }]);
+    res.json(requests);
 });
 
-app.post('/request', function(req, res) {
-    res.json([{
-        id: 2,
-        creatorName: 'creator',
-        clientName: 'client',
-        clientId: '123456789',
-        phoneNum: '0547776666',
-        address: '123 main street',
-        desc: 'description',
-        decisions: 'decisions',
-        status: 'new',
-        category: 'elderly'
-    }]);
+app.post('/request', jsonParser, function(req, res) {
+
+    console.log(req.body);
+
+    const newRequest = {
+        ...req.body,
+        id: id++
+    };
+
+    requests.push(newRequest);
+
+    res.json(newRequest);
 });
 
 app.listen(port, function() {
